440a112cdc78d6b280515305a2908c1fb99c5080
unknown <kartikey.porwal@gmail.com>
2020-12-27 01:40:31 +0530

    testing new changes

--- naukri_resume_update.py
+++ naukri_resume_update.py
@@ -6,9 +6,11 @@ from webdriver_manager.chrome import ChromeDriverManager
 gChromeOptions = webdriver.ChromeOptions()
 gChromeOptions.add_argument("window-size=1920x1480")
 gChromeOptions.add_argument("disable-dev-shm-usage")
+gChromeOptions.add_argument("headless")
 gDriver = webdriver.Chrome(
     chrome_options=gChromeOptions, executable_path=ChromeDriverManager().install()
 )
+print(f"opening python website")
 gDriver.get("https://www.python.org/")
 time.sleep(3)
 gDriver.save_screenshot("my_screenshot.png")
